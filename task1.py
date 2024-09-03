fc63cef9d3e6e66d758ea7a3befc345ed6a6eb48
bykva98 <bykva.98@mail.ru>
2024-09-03 23:44:44 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,20 @@
+# Task 1
+print("1st program")
+print(9 ** 0.5 * 5)
+
+# Task 2
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# Task 3
+print("3rd program")
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+print((2 * 2 + 2) >= (2 * (2 + 2)))
+print((2 * 2 + 2) <= (2 * (2 + 2)))
+
+# Task 4
+print("4th program")
+print('123.456')
+print(int(float('123.456') * 10 % 10))
